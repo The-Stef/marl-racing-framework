10db6873000300e377f720cf507231d39aa8beba
The-Stef <145986755+The-Stef@users.noreply.github.com>
2026-05-11 01:45:30 +0200

    Fully set up v1 of the get_obs() function

--- marl_racing_env/env/helpers/helpers.py
+++ marl_racing_env/env/helpers/helpers.py
@@ -16,26 +16,53 @@ def current_tile(env, agent):
     tile = int(theta / (2 * np.pi) * env.NUM_TILES)
     return tile
 
-def get_obs(self, agent):
+def get_obs(env, agent):
     """Return the agent's observation. Values computed using info straight from the Car object."""
 
-    velocity_x = float(self.CARS[agent].hull.linearVelocity[0])
-    velocity_y = float(self.CARS[agent].hull.linearVelocity[1])
+    velocity_x = float(env.CARS[agent].hull.linearVelocity[0])
+    velocity_y = float(env.CARS[agent].hull.linearVelocity[1])
     velocity = np.sqrt(velocity_x ** 2 + velocity_y ** 2)
 
-    # current_direction = self._car_heading()
-    # desired_direction = self._compute_desired_direction()
-    # heading_error = self._wrap_angle(desired_direction - current_direction)
+    current_direction = car_heading(env, agent)
+    desired_direction = compute_desired_direction(env, agent)
+    heading_error = wrap_angle(desired_direction - current_direction)
 
     radial_error = np.clip(
-        self._compute_radial_error(),
-        -self.TRACK_HALF_WIDTH,
-        self.TRACK_HALF_WIDTH
+        compute_radial_error(env, agent),
+        -env.TRACK_HALF_WIDTH,
+        env.TRACK_HALF_WIDTH
     )
 
-    angular_velocity = float(self.CARS[agent].hull.angularVelocity)
+    angular_velocity = float(env.CARS[agent].hull.angularVelocity)
 
     return np.array(
         [velocity, heading_error, radial_error, angular_velocity],
         dtype=np.float32
     )
+
+def wrap_angle(angle):
+    """Wrap angle in [-pi, pi]."""
+    return (angle + np.pi) % (2 * np.pi) - np.pi
+
+def car_heading(env, agent):
+    """Return the current car's real forward heading."""
+    return wrap_angle(float(env.CARS[agent].hull.angle) + np.pi / 2)
+
+def compute_desired_direction(env, agent):
+    """Return the tangent direction angle the current car should follow."""
+    rx = env.CARS[agent].hull.position[0] - env.TRACK_CENTER_X
+    ry = env.CARS[agent].hull.position[1] - env.TRACK_CENTER_Y
+
+    # Clockwise tangent, center-to-car position rotated by 90 degrees to right
+    tx = ry
+    ty = -rx
+
+    return np.arctan2(ty, tx)
+
+def compute_radial_error(env, agent):
+    """Compute signed distance from the ideal circular centerline."""
+    distance_from_center = np.sqrt(
+        (env.TRACK_CENTER_X - env.CARS[agent].hull.position[0]) ** 2 +
+        (env.TRACK_CENTER_Y - env.CARS[agent].hull.position[1]) ** 2
+    )
+    return distance_from_center - env.TRACK_RADIUS
